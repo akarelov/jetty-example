c110936d346eae601df490c0b96ccad9f85dde84
akarelov <karelov.artyom@gmail.com>
2020-02-21 15:18:10 +0400

    refactoring singleton scope for service which we inject to servlets

--- src/main/java/com/akarelov/jetty/configuration/NonServletModuleConfiguration.java
+++ src/main/java/com/akarelov/jetty/configuration/NonServletModuleConfiguration.java
@@ -5,6 +5,7 @@ import com.akarelov.jetty.service.ServiceImpl;
 import com.akarelov.jetty.servlet.CustomSecondServlet;
 import com.akarelov.jetty.servlet.CustomServlet;
 import com.google.inject.AbstractModule;
+import com.google.inject.Scopes;
 import com.google.inject.name.Names;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,7 +13,7 @@ import org.slf4j.LoggerFactory;
 public class NonServletModuleConfiguration extends AbstractModule {
     @Override
     protected void configure() {
-        bind(Service.class).to(ServiceImpl.class);
+        bind(Service.class).to(ServiceImpl.class).in(Scopes.SINGLETON);
         bind(Logger.class).annotatedWith(Names.named("secondLogger")).toInstance(LoggerFactory.getLogger(CustomSecondServlet.class));
         bind(Logger.class).annotatedWith(Names.named("firstLogger")).toInstance(LoggerFactory.getLogger(CustomServlet.class));
     }
--- src/main/java/com/akarelov/jetty/service/ServiceImpl.java
+++ src/main/java/com/akarelov/jetty/service/ServiceImpl.java
@@ -1,8 +1,5 @@
 package com.akarelov.jetty.service;
 
-import com.google.inject.Singleton;
-
-@Singleton
 public class ServiceImpl implements Service {
     @Override
     public String print() {
